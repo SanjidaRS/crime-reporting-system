3986d7fcce578970f2fc223989be3ddf2b552a45
Sanjida Rahman Supti <166355719+SanjidaRS@users.noreply.github.com>
2026-07-06 15:48:05 +0000

    changed

--- backend/app/utils/report_id.py
+++ backend/app/utils/report_id.py
@@ -4,11 +4,7 @@ report_counter = 1
 
 def generate_report_id():
     global report_counter
-
     year = datetime.now().year
-
     report_id = f"CR-{year}-{report_counter:06d}"
-
     report_counter += 1
-
     return report_id
